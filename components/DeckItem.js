e36d63797b867227d6c174f6e8c55f34c18b7194
SamDegDev <samdegdev@outlook.com>
2017-11-05 21:25:19 +0000

    feat: added style

--- components/DeckItem.js
+++ components/DeckItem.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { white, black, gray } from '../utils/colors';
 
 export default function DeckItem({ title, questions }) {
+  const total = questions.length;
+
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{questions.length} cards</Text>
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{total} card{total == 1 ? 's' : ''}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 2,
+  },
+  title: {
+    fontSize: 25,
+    color: black,
+    marginBottom: 5,
+  },
+  subtitle: {
+    fontSize: 15,
+    color: gray,
+  }
+});

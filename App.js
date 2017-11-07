a89219ce7316f5d85bb7410ee011df56ea9cb3ce
SamDegDev <samdegdev@outlook.com>
2017-11-07 22:15:25 +0000

    feat: added Redux support

--- App.js
+++ App.js
@@ -3,14 +3,21 @@ import { View } from 'react-native';
 import { white, black } from './utils/colors';
 import { FlashStatusBar } from './utils/statusbar';
 import { MainNavigator } from './utils/navigator';
+//
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <FlashStatusBar backgroundColor={white} barStyle='light-content' />
-        <MainNavigator />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <FlashStatusBar backgroundColor={white} barStyle='light-content' />
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }

b69e019eff7e1777ee2844ed009ce8ad7516f971
SamDegDev <samdegdev@outlook.com>
2017-11-07 22:18:31 +0000

    fix: various fixes
    - removed the id as parameter
    - fixed onPress function replacing the id with the title as key
    - fixed the total plural string

--- components/DeckItem.js
+++ components/DeckItem.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { white, black, gray } from '../utils/colors';
 
-export default function DeckItem({ id, title, questions, showDeck }) {
+export default function DeckItem({ title, questions, showDeck }) {
   const total = questions.length;
 
   // when this item gets pressed
   onPress = function() {
     // shows the cards for this deck
-    showDeck(id);
+    showDeck(title);
   }
 
   return (
     <TouchableWithoutFeedback onPress={this.onPress}>
       <View style={styles.item}>
         <Text style={styles.title}>{title}</Text>
-        <Text style={styles.subtitle}>{total} card{total == 1 ? 's' : ''}</Text>
+        <Text style={styles.subtitle}>{total} card{total == 1 ? '' : 's'}</Text>
       </View>
     </TouchableWithoutFeedback>
   );

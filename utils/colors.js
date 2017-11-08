e67b12812dba50fda05862b714078bba2eeb2a67
SamDegDev <samdegdev@outlook.com>
2017-11-08 21:02:33 +0000

    feat: added new colors

--- utils/colors.js
+++ utils/colors.js
@@ -1,4 +1,7 @@
-export const blue = '#0f6ec8';
 export const white = '#ffffff';
 export const black = '#000000';
+export const blue = '#0073e5';
 export const gray = '#757575'
+export const green = '#008000';
+export const red = '#ff0000';
+export const yellow = '#ffbf00';

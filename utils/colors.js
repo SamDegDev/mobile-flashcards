aa7f9af431e844758ec28391e65303fe6264865e
SamDegDev <samdegdev@outlook.com>
2017-11-10 21:30:01 +0000

    feat: added lightGray color

--- utils/colors.js
+++ utils/colors.js
@@ -1,7 +1,8 @@
 export const white = '#ffffff';
 export const black = '#000000';
 export const blue = '#0073e5';
-export const gray = '#757575'
+export const gray = '#a0a0a0'
 export const green = '#008000';
 export const red = '#ff0000';
 export const yellow = '#ffbf00';
+export const lightGray = '#DCDCDC';

6190159261f06bdfb2f962b016cfe7615eb2486c
SamDegDev <samdegdev@outlook.com>
2017-11-11 19:26:50 +0000

    feat: added view for empty stack

--- components/DecksList.js
+++ components/DecksList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { AppLoading } from 'expo';
 import DeckItem from './DeckItem';
 import { connect } from 'react-redux';
 import { fetchDecks, addDeck } from '../utils/FlashcardsAPI';
 import { receiveDecks } from '../actions';
+import TextButton from './TextButton';
+import { white, black } from '../utils/colors';
 
 class DecksList extends Component {
   state = {
@@ -32,12 +34,30 @@ class DecksList extends Component {
     );
   }
 
+  handleAddDeckClick = () => {
+    this.props.navigation.navigate(
+      'AddDeck'
+    );
+  }
+
   render() {
     const { decks } = this.props;
     const { ready } = this.state;
 
     if (ready === false) {
       return <AppLoading />
+    }
+
+    // the object is empty
+    if (Object.keys(decks).length === 0) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.title}>There are no Decks!</Text>
+          <TextButton onPress={this.handleAddDeckClick}>
+          {'Add a Deck'.toUpperCase()}
+        </TextButton>
+        </View>
+      );
     }
 
     return(
@@ -52,6 +72,23 @@ class DecksList extends Component {
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 25,
+    color: black,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+});
 
 function mapStateToProps(decks) {
   return {
